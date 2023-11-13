9b04cf5970b981a927fa13db3d2787e11e39c787
MZhann <zhanbolat.mukan2004@gmail.com>
2023-11-13 19:56:42 +0600

    pages, import fonts, create basic components

---
+++ frontend/components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => (
+    <footer>
+        <h1>this is Footer!!!</h1>
+    </footer>
+);
+
+export default Footer;
---
+++ frontend/pages/about.js
@@ -0,0 +1,9 @@
+const About = () =>(
+    <>
+        <h1>КАТАЛОГ каталог</h1>
+        <p>Cillum labore in ipsum nisi non cupidatat occaecat Lorem occaecat adipisicing laborum.</p>
+    
+    </>
+)
+
+export default About;
