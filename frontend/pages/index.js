85eed98c9567cff49ab4ed6053b2cd06a61695ea
MZhann <zhanbolat.mukan2004@gmail.com>
2023-11-14 19:45:03 +0600

    create Companies.js component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,10 @@
 import styles from '../styles/Home.module.css'
 import News from '@/components/News';
+import Companies from '@/components/Companies';
 const Home = () => (
   <>
     <News/>
-
+    <Companies/>
   
   </>
 )
