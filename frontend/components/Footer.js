664a2457b4c90f8f36ce6846569896b4de31d9ba
syrhundred <madiev.inc@gmail.com>
2023-11-14 17:36:11 +0600

    Footer was added

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,7 +1,32 @@
-const Footer = () => (
-    <footer>
-        <h1>this is Footer!!!</h1>
-    </footer>
-);
+import React from "react"
+import Image from "next/image"
+import logo from "../public/images/image 1.svg"
+//width: 1110px;
+//height: 219px;
+//background-color: white;
+//bottom: 0;
+//left: 0;
+//right: 0;
+//position: fixed;
+//margin: 0 auto;
+
+const Footer = () => {
+    return(
+        <div>
+            <div className="w-full justify-center flex absolute bottom-0">
+                <div className="h-40 bg-white w-5/6 flex justify-between">
+                    <Image className="self-center" src={logo} alt="logo"/>
+                    <div className="ProductSansThin">
+                        <p>ИНТЕРНЕТ МАГАЗИН</p>
+                        <br/>
+                        <p className="pb-1.5">О компании</p>
+                        <p className="pb-1.5">Доставка</p>
+                        <p className="pb-1.5">Реквизиты</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Footer;
