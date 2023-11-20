3c05808d97c0d2e1d2e0a189393658dac8ce5325
MZhann <zhanbolat.mukan2004@gmail.com>
2023-11-20 15:01:37 +0600

    start carousel component

--- frontend/components/News.js
+++ frontend/components/News.js
@@ -1,8 +1,14 @@
+import Carousel from "./Carousel";
+
 const News = () => (
     <div className="w-full h-80 pl-30 pr-30  flex justify-center mt-5">
         <div className="w-3/4  flex" >
 
-            <div className="h-full w-3/4 bg-orange-400 mr-4 bg-karouselImage bg-cover bg-center rounded"></div>
+            <div className="h-full w-3/4 bg-orange-400 mr-4 bg-karouselImage bg-cover bg-center rounded">
+            {/* <div className="h-full w-3/4 mr-4 rounded"> */}
+                <Carousel />
+            </div>
+            {/* </div> */}
             <div className="flex flex-col justify-between">
                 <div className="h-1/2 w-96  bg-orange-200 bg-equipmentImage bg-cover bg-center rounded"></div>
                 <div className="h-4"></div>
