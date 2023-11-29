68578d1e939263f04f60f1fa2dbeadfc11ac612b
MZhann <zhanbolat.mukan2004@gmail.com>
2023-11-29 21:23:45 +0600

    feat: create catalog products rendering from db.json

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -5,21 +5,29 @@ import Filter from "@/components/Filter";
 import Catalog from "@/components/Catalog";
 import Pagination from "@/components/Pagination"
 
+export const getStaticProps = async () => {
+    const res = await fetch ('http://localhost:5000/products');
+    const data = await res.json();
 
-const About = () =>(
-    <div className="h-auto w-full  p-10 flex flex-col items-center">
-        <div className="w-4/5  h-full flex">
-            <Filter />
-            <Catalog />
+    // console.log('data >>> ', data)
+
+    return {
+        props: { products: data }
+    }
+}
+
+const About = (props) =>{
+    // console.log("props >>> ", props)
+    
+    return (
+        <div className="h-auto w-full  p-10 flex flex-col items-center">
+            <div className="w-4/5  h-full flex">
+                <Filter />
+                <Catalog products={props.products} />
+            </div>
+            <Pagination/>
         </div>
-
-        <Pagination/>
-        
-       
-
-        
-
-    </div>
-)
+    )
+}
 
 export default About;
