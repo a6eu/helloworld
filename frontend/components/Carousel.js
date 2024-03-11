b5702a6a0d5dbd48bb34095fb0941e024e90c091
Yeboka <89529214+Yeboka@users.noreply.github.com>
2024-03-11 20:00:20 +0500

    add logic to carousel

--- frontend/components/Carousel.js
+++ frontend/components/Carousel.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import { Carousel } from 'antd';
+import React, {useEffect, useState} from 'react';
+import {Carousel, Image} from 'antd';
+import axios from "axios";
+import banner from "../public/images/banner.png";
 
-const array = [1, 2, 3, 4]
-const App = () => (
-    <Carousel autoplay>
-        {array.map(
-            item => (
-                <div>
-                    <h3 className='h-[375px] text-[#fff] leading-[160px] text-center bg-[#364d79] min-[320px]:max-md:h-[250px] min-[320px]:max-lg:h-[300px]'>{item}</h3>
-                </div>
-            )
-        )}
-    </Carousel>
-);
+const apiUrl = 'https://shop-01it-group.up.railway.app/api/v1/news/'
+const App = () => {
+
+    const [newsList, setNewsList] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+    const fetchData = () => {
+        axios.get(apiUrl)
+            .then((response) => {
+                let res = response.data.results.slice(0, 3)
+                setNewsList(res);
+                console.log('response:', response.data.results.slice(0, 3));
+            })
+            .catch((e) => {
+                console.log(e);
+            })
+    }
+    return (
+        <Carousel autoplay>
+            {newsList.map(
+                item => (
+                    <div>
+                        {/*<h3 className='h-[375px] text-[#fff] leading-[160px] text-center bg-[#364d79] min-[320px]:max-md:h-[250px] min-[320px]:max-lg:h-[300px]'>{item}</h3>*/}
+                        {item.image ?
+                            <Image preview={false} style={{width: '100%', height: 300}} src={item.image}/>
+                        :
+                            <Image preview={false} style={{width: '100%'}} alt={"Banner"}/>
+                        }
+                    </div>
+                )
+            )}
+        </Carousel>
+    );
+}
 export default App;
 
--- frontend/components/NewsCard.js
+++ frontend/components/NewsCard.js
@@ -1,25 +1,40 @@
 import Image from "next/image";
 
-const NewsCard = ({news}) => (
-    <div className="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-3 m-2 w-full max-w-2xl">
-        <div className="w-full flex flex-col border border-gray-300 rounded-md overflow-hidden mt-6">
-            <div className="relative">
-                {
-                    news.image &&
-                    <Image src={news.image} alt="News Thumbnail" width={"670"} height={400}/>
+const NewsCard = ({news}) => {
 
-                }
-            </div>
-            <div className="flex flex-col p-4">
-                <p className="text-gray-800 text-xl">{news.title}</p>
+    const getTimeFormatted = (time) => {
+        let arr = time.split(/[A-Z]/)
+        let formattedTime = '';
 
-                <p className="text-gray-500 text-md mt-2">
-                    {news.content}
-                </p>
-                {/*<p className="text-gray-500 text-xs mt-4">time</p>*/}
+        arr.map((item) => {
+           formattedTime += item + `    `;
+        });
+
+        return formattedTime;
+    };
+
+    return (
+        <div className="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-3 m-2 w-full max-w-2xl">
+            <div className="w-full flex flex-col border border-gray-300 rounded-md overflow-hidden mt-6">
+                <div className="relative">
+                    {
+                        news.image &&
+                        <Image src={news.image} alt="News Thumbnail" width={"670"} height={400}/>
+
+                    }
+                </div>
+                <div className="flex flex-col p-4">
+                    <p className="text-gray-800 text-xl">{news.title}</p>
+
+                    <p className="text-gray-500 text-md mt-2">
+                        {news.content}
+                    </p>
+                    <p className="text-gray-500 text-xs mt-4">{getTimeFormatted(news.created_at)}</p>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+}
+
 
 export default NewsCard;
