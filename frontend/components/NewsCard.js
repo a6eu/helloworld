239d2c87def4e2a686f2fa19130095f55c330fc8
Yeboka <89529214+Yeboka@users.noreply.github.com>
2024-03-11 18:39:52 +0500

    add backend connection to news page and infinite scroll

--- frontend/components/NewsCard.js
+++ frontend/components/NewsCard.js
@@ -1,18 +1,22 @@
 import Image from "next/image";
 
-const NewsCard = ({ imageSrc, title, content, time }) => (
+const NewsCard = ({news}) => (
     <div className="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-3 m-2 w-full max-w-2xl">
         <div className="w-full flex flex-col border border-gray-300 rounded-md overflow-hidden mt-6">
             <div className="relative">
-                <Image src={imageSrc} alt="News Thumbnail" width={"670"} height={400} />
+                {
+                    news.image &&
+                    <Image src={news.image} alt="News Thumbnail" width={"670"} height={400}/>
+
+                }
             </div>
             <div className="flex flex-col p-4">
-                    <p className="text-gray-800 text-xl">{title}</p>
+                <p className="text-gray-800 text-xl">{news.title}</p>
 
                 <p className="text-gray-500 text-md mt-2">
-                    {content}
+                    {news.content}
                 </p>
-                <p className="text-gray-500 text-xs mt-4">{time}</p>
+                {/*<p className="text-gray-500 text-xs mt-4">time</p>*/}
             </div>
         </div>
     </div>
--- frontend/pages/news.js
+++ frontend/pages/news.js
@@ -1,37 +1,62 @@
-import React from 'react';
+"use client"
+
+import React, {useEffect, useState} from 'react';
 import Image from 'next/image';
 import NewsCard from "@/components/NewsCard";
 import MainContainer from "@/components/MainContainer";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+import {Divider, Skeleton} from "antd";
 
 
+const apiUrl = 'https://shop-01it-group.up.railway.app/api/v1/news/'
 const News = () => {
-  const newsArray = [
-    {
-      id: 1,
-      imageSrc: '/images/news.jpg', 
-      title: 'Sample News Title 1',
-      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-      time: '2 hours ago',
-    },
-    {
-      id: 2,
-      imageSrc: '/images/news.jpg', 
-      title: 'Sample News Title 2',
-      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-      time: '1 hour ago',
-    },
-  ];
+    const [newsList, setNewsList] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [loading, setLoading] = useState(false);
 
-  return (
-      <MainContainer>
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-        <div className="w-full flex flex-col items-center">
-          {newsArray.map((news) => (
-              <NewsCard key={news.id} {...news} />
-          ))}
-        </div>
-      </MainContainer>
-  );
+    const fetchData = () => {
+        setLoading(true);
+        axios.get(apiUrl + `?page=${currentPage}`)
+            .then((response) => {
+                setNewsList([...newsList, ...response.data.results]);
+                setTotal(response.data.count);
+                console.log('response:', response.data);
+                setCurrentPage(currentPage + 1);
+            })
+            .catch((e) => {
+                console.log(e);
+            })
+        setLoading(false);
+    }
+
+
+    return (
+        <MainContainer>
+            <div className={'w-full flex flex-col items-center'} id={"infiniteScroll"}>
+                <InfiniteScroll
+                    dataLength={newsList.length}
+                    next={fetchData}
+                    hasMore={newsList.length < total}
+                    loader={<Skeleton active />}
+                    endMessage={<Divider>🎉 Вы просмотрели все новости! 🎉</Divider>}
+                    scrollableTarget="infiniteScroll"
+                >
+                    {newsList.map((news) => (
+                        <NewsCard key={news.id} news={news} />
+                    ))}
+                </InfiniteScroll>
+                <div className="w-full flex flex-col items-center">
+
+                </div>
+            </div>
+        </MainContainer>
+    );
 };
 
 export default News;
