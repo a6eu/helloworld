d56c3924d2b75297e74810fb1b6362df41ad4f2b
syrhundred <madiev.inc@gmail.com>
2024-03-31 16:59:06 +0500

    Добавил кнопку выхода в модальном окне

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -21,8 +21,8 @@ import defaultImage from "@/public/images/picture.png";
 import {AlertContext} from "@/components/AlertContext";
 import {debounce} from "lodash";
 import {config} from "@/config";
-import { getSession } from "@/login";
-import { useCookies } from "react-cookie";
+import {getSession} from "@/login";
+import {useCookies} from "react-cookie";
 
 const Header = () => {
     useTokenExpirationCheck()
@@ -33,7 +33,7 @@ const Header = () => {
     const [isSearchVisible, setIsSearchVisible] = useState(false);
     const searchContainerRef = useRef(null);
     const Search = Input;
-    const [cookies] = useCookies(['session']); 
+    const [cookies] = useCookies(['session']);
     const loggedInItems = [{
         label: <a href={'/profile'}>Профиль</a>, key: '0',
     }, {
@@ -110,10 +110,10 @@ const Header = () => {
             if (!session) {
                 console.log("session not found")
             }
-             const accessToken = session?.user.accessToken
-            if(accessToken){
+            const accessToken = session?.user.accessToken
+            if (accessToken) {
                 setIsLogged(true);
-            }else{
+            } else {
                 setIsLogged(false);
             }
         };
@@ -121,7 +121,7 @@ const Header = () => {
         fetchCookie();
     }, []);
 
-    
+
     useEffect(() => {
         if (isLogged) {
             setProfileImg(profileIconNonActive)
@@ -158,148 +158,152 @@ const Header = () => {
         };
     }, []);
 
-    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     function openModal() {
-        setIsModalOpen(true)
+        setIsModalOpen(true);
+    }
+
+    function closeModal() {
+        setIsModalOpen(false);
     }
 
     return (
         <header className={styles.header}>
-        <div className={"max-w-screen-xl w-full flex justify-between items-center p-2"}>
-            <Link href={"/"} className='min-[320px]:max-[880px]:hidden'>
-                <Image className="cursor-pointer min-w-[60px]"
-                       src="/images/image 1.svg"
-                       height={60}
-                       width={60}
-                       alt="logo"
-                />
-            </Link>
-            <CategoriesDropdown/>
-            <div className={"flex items-center"} ref={searchContainerRef}>
-                <Search placeholder="Поиск"
-                        onChange={handleSearchChange}
-                        value={searchTerm}
-                        className='w-[400px] min-[320px]:max-lg:w-auto'
-                />
-                {isSearchVisible && searchTerm.trim() !== '' && (
+            <div className={"max-w-screen-xl w-full flex justify-between items-center p-2"}>
+                <Link href={"/"} className='min-[320px]:max-[880px]:hidden'>
+                    <Image className="cursor-pointer min-w-[60px]"
+                           src="/images/image 1.svg"
+                           height={60}
+                           width={60}
+                           alt="logo"
+                    />
+                </Link>
+                <CategoriesDropdown/>
+                <div className={"flex items-center"} ref={searchContainerRef}>
+                    <Search placeholder="Поиск"
+                            onChange={handleSearchChange}
+                            value={searchTerm}
+                            className='w-[400px] min-[320px]:max-lg:w-auto'
+                    />
+                    {isSearchVisible && searchTerm.trim() !== '' && (
 
-                    <ul className="absolute max-h-[62vh] overflow-auto z-50 bg-white w-[438px] top-[59px] rounded shadow-lg border border-b-blue-200">
-                        {searchResults.map((product) => (
-                            <Link rel="stylesheet" href={`/products/${product.name}`} key={product.id}>
-                                <li className="flex flex-row border justify-between border-t-o border-l-0 border-r-0 py-2 px-2"
+                        <ul className="absolute max-h-[62vh] overflow-auto z-50 bg-white w-[438px] top-[59px] rounded shadow-lg border border-b-blue-200">
+                            {searchResults.map((product) => (
+                                <Link rel="stylesheet" href={`/products/${product.name}`} key={product.id}>
+                                    <li className="flex flex-row border justify-between border-t-o border-l-0 border-r-0 py-2 px-2"
                                     >
-                                    <div className="flex flex-row h-[85px] w-[55%]">
-                                        {product.img_url ?
-                                            <Image
-                                                width={80}
-                                                height={80}
-                                                alt={"pic"}
-                                                src={product.img_url}
-                                                className={`object-contain`}
-                                            /> :
-                                            <Image src={defaultImage} alt={product.name}
-                                                   width={80}
-                                                   height={80}/>
-                                        }
-                                        <div className="ml-2">
-                                            <h3 className="text-[13px]">{truncateDescription(product.name, 20)}</h3>
-                                            <span>{parseInt(product.price)} ₸</span>
+                                        <div className="flex flex-row h-[85px] w-[55%]">
+                                            {product.img_url ?
+                                                <Image
+                                                    width={80}
+                                                    height={80}
+                                                    alt={"pic"}
+                                                    src={product.img_url}
+                                                    className={`object-contain`}
+                                                /> :
+                                                <Image src={defaultImage} alt={product.name}
+                                                       width={80}
+                                                       height={80}/>
+                                            }
+                                            <div className="ml-2">
+                                                <h3 className="text-[13px]">{truncateDescription(product.name, 20)}</h3>
+                                                <span>{parseInt(product.price)} ₸</span>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <p className="text-gray-400 text-xs w-[40%]">{truncateDescription(product.description, 105)}</p>
-                                </li>
-                            </Link>))}
-                    </ul>)}
-            </div>
-            <CityDropdownMenu
-                selectedCity={selectedCity}
-                onCityChange={handleCityChange}
-            />
+                                        <p className="text-gray-400 text-xs w-[40%]">{truncateDescription(product.description, 105)}</p>
+                                    </li>
+                                </Link>))}
+                        </ul>)}
+                </div>
+                <CityDropdownMenu
+                    selectedCity={selectedCity}
+                    onCityChange={handleCityChange}
+                />
 
-            <div className='min-[320px]:max-[880px]:flex hidden pr-2'>
-                <Dropdown
-                    menu={{
-                        items: isLogged ? loggedInItems : loggedOutItems,
-                    }}
-                    trigger={['click']}
-                >
-                    <a onClick={(e) => e.preventDefault()}>
-                        <svg width="35px" height="35px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-                             fill="#1075b2">
-                            <g id="SVGRepo_iconCarrier">
-                                <g>
-                                    <path d="M4 19h16v-2H4v2zm16-6H4v2h16v-2zM4 9v2h16V9H4zm16-4H4v2h16V5z"/>
+                <div className='min-[320px]:max-[880px]:flex hidden pr-2'>
+                    <Dropdown
+                        menu={{
+                            items: isLogged ? loggedInItems : loggedOutItems,
+                        }}
+                        trigger={['click']}
+                    >
+                        <a onClick={(e) => e.preventDefault()}>
+                            <svg width="35px" height="35px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                                 fill="#1075b2">
+                                <g id="SVGRepo_iconCarrier">
+                                    <g>
+                                        <path d="M4 19h16v-2H4v2zm16-6H4v2h16v-2zM4 9v2h16V9H4zm16-4H4v2h16V5z"/>
+                                    </g>
                                 </g>
-                            </g>
-                        </svg>
-                    </a>
-                </Dropdown>
+                            </svg>
+                        </a>
+                    </Dropdown>
+                </div>
+
+                <div className='flex items-center justify-between w-[120px] min-[320px]:max-[880px]:hidden'>
+                    <Link href={"/favorites"} className='mb-[5px]'>
+                        <Image className="cursor-pointer min-w-[22px]"
+                               src={favoritesImg}
+                               height={22}
+                               width={22}
+                               alt="favourites"
+                               onMouseOver={() => setFavoritesImg(favIconActive)}
+                               onMouseLeave={() => setFavoritesImg(favIconNonActive)}
+                        />
+                    </Link>
+                    <Link className='mb-[5px]' href={"/cart"}>
+                        <Image className="cursor-pointer min-w-[30px]"
+                               src={cartImg}
+                               height={30}
+                               width={30}
+                               alt="cart"
+                               onMouseOver={() => setCartImg(cartIconActive)}
+                               onMouseLeave={() => setCartImg(cartIconNonActive)}
+                        />
+                    </Link>
+
+                    {!isLogged ? <div
+                        onClick={openModal}
+                        className='flex justify-between'
+                    >
+                        <Image
+                            className="cursor-pointer min-w-[35px]"
+                            src={loginIconImg}
+                            height={35}
+                            width={35}
+                            alt="profile"
+                            onMouseOver={() => {
+                                isLogged ? setLoginIconImg(profileIconActive) : setLoginIconImg(loginIconActive)
+                            }}
+                            onMouseLeave={() => {
+                                isLogged ? setLoginIconImg(profileIconNonActive) : setLoginIconImg(loginIconNonActive)
+                            }}
+                        />
+                    </div> : <Link href={"/profile"}>
+                        <Image
+                            className="cursor-pointer"
+                            src={profileImg}
+                            height={35}
+                            width={35}
+                            alt="profile"
+                            onMouseOver={() => setProfileImg(profileIconActive)}
+                            onMouseLeave={() => setProfileImg(profileIconNonActive)}
+                        />
+                    </Link>}
+                </div>
             </div>
+            {alert.show &&
+                <div
+                    className={`fixed z-50 top-5 right-5 transition-opacity duration-300 ease-out transform-gpu ${
+                        alert.show ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-10'
+                    }`}><Alert type={alert.type} showIcon
+                               className={'flex flex-row w-[340px]'}
+                               message={alert.message} closable></Alert></div>}
+            <ModalDialog isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
+        </header>
 
-            <div className='flex items-center justify-between w-[120px] min-[320px]:max-[880px]:hidden'>
-                <Link href={"/favorites"} className='mb-[5px]'>
-                    <Image className="cursor-pointer min-w-[22px]"
-                           src={favoritesImg}
-                           height={22}
-                           width={22}
-                           alt="favourites"
-                           onMouseOver={() => setFavoritesImg(favIconActive)}
-                           onMouseLeave={() => setFavoritesImg(favIconNonActive)}
-                    />
-                </Link>
-                <Link className='mb-[5px]' href={"/cart"}>
-                    <Image className="cursor-pointer min-w-[30px]"
-                           src={cartImg}
-                           height={30}
-                           width={30}
-                           alt="cart"
-                           onMouseOver={() => setCartImg(cartIconActive)}
-                           onMouseLeave={() => setCartImg(cartIconNonActive)}
-                    />
-                </Link>
-
-                {!isLogged ? <div
-                    onClick={openModal}
-                    className='flex justify-between'
-                >
-                    <Image
-                        className="cursor-pointer min-w-[35px]"
-                        src={loginIconImg}
-                        height={35}
-                        width={35}
-                        alt="profile"
-                        onMouseOver={() => {
-                            isLogged ? setLoginIconImg(profileIconActive) : setLoginIconImg(loginIconActive)
-                        }}
-                        onMouseLeave={() => {
-                            isLogged ? setLoginIconImg(profileIconNonActive) : setLoginIconImg(loginIconNonActive)
-                        }}
-                    />
-                </div> : <Link href={"/profile"}>
-                    <Image
-                        className="cursor-pointer"
-                        src={profileImg}
-                        height={35}
-                        width={35}
-                        alt="profile"
-                        onMouseOver={() => setProfileImg(profileIconActive)}
-                        onMouseLeave={() => setProfileImg(profileIconNonActive)}
-                    />
-                </Link>}
-            </div>
-        </div>
-        {alert.show &&
-            <div
-                className={`fixed z-50 top-5 right-5 transition-opacity duration-300 ease-out transform-gpu ${
-                    alert.show ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-10'
-                }`}><Alert type={alert.type} showIcon
-                           className={'flex flex-row w-[340px]'}
-                           message={alert.message} closable></Alert></div>}
-        <ModalDialog isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
-    </header>
-
-)
+    )
 };
 
 export default Header;
--- frontend/components/ModalDialog.js
+++ frontend/components/ModalDialog.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { Dialog } from '@headlessui/react';
+import React, {useState, Fragment} from 'react';
+import {Dialog, Transition} from '@headlessui/react';
 import SignUpForm from '@/components/SignUpForm';
 import LogInForm from '@/components/LogInForm';
 import ForgotPasswordForm from "@/components/ForgotPasswordForm";
 
-function ModalDialog({ isModalOpen, setIsModalOpen }) {
+function ModalDialog({isModalOpen, setIsModalOpen}) {
     const [formType, setFormType] = useState('login');
 
     function openSignUpForm() {
@@ -20,47 +20,78 @@ function ModalDialog({ isModalOpen, setIsModalOpen }) {
         setFormType('forgot');
     }
 
+
     return (
-        <Dialog
-            as="div"
-            className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-20 z-50"
-            open={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-        >
-            <Dialog.Panel className="fixed flex justify-center items-center inset-0 overflow-auto">
-                <div className="bg-white rounded-lg p-5 w-full max-w-md">
-                    {
+        <Transition
+            show={isModalOpen}
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-95 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+            as={Fragment}>
+            <Dialog
+                as="div"
+                className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-20 z-50"
+                open={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+            >
+                <Dialog.Panel className="fixed flex justify-center items-center inset-0 overflow-auto">
+                    <div className="bg-white rounded-lg p-5 w-full max-w-md">
                         {
-                            'signup': (
-                                <>
-                                    <Dialog.Title className="text-[#1075B2] text-xl mb-6 text-center">
-                                        Зарегистрируйтесь, чтобы быть круче
-                                    </Dialog.Title>
-                                    <SignUpForm onLogInClick={openLoginForm} />
-                                </>
-                            ),
-                            'login': (
-                                <>
-                                    <Dialog.Title className="text-[#1075B2] text-xl mb-6 text-center">
-                                        Добро пожаловать!
-                                    </Dialog.Title>
-                                    <LogInForm onSignUpClick={openSignUpForm} onForgotClick={openForgotPasswordForm}
-                                               setIsModalOpen={setIsModalOpen} />
-                                </>
-                            ),
-                            'forgot': (
-                                <>
-                                    <Dialog.Title className="text-[#1075B2] text-xl mb-6 text-center">
-                                        Восстановить пароль
-                                    </Dialog.Title>
-                                    <ForgotPasswordForm onLogInClick={openLoginForm} />
-                                </>
-                            )
-                        }[formType]
-                    }
-                </div>
-            </Dialog.Panel>
-        </Dialog>
+                            {
+                                'signup': (
+                                    <>
+                                        <div className='flex items-center justify-between mb-6'>
+                                            <Dialog.Title className="text-[#1075B2] text-xl text-center pl-5">
+                                                Зарегистрируйтесь, чтобы быть круче
+                                            </Dialog.Title>
+                                            <button onClick={() => setIsModalOpen(false)}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20"
+                                                     height="20" viewBox="0 0 50 50">
+                                                    <path
+                                                        d="M 7.7070312 6.2929688 L 6.2929688 7.7070312 L 23.585938 25 L 6.2929688 42.292969 L 7.7070312 43.707031 L 25 26.414062 L 42.292969 43.707031 L 43.707031 42.292969 L 26.414062 25 L 43.707031 7.7070312 L 42.292969 6.2929688 L 25 23.585938 L 7.7070312 6.2929688 z"></path>
+                                                </svg>
+                                            </button>
+                                        </div>
+                                        <SignUpForm onLogInClick={openLoginForm}/>
+                                    </>
+                                ),
+                                'login': (
+                                    <>
+                                        <div className='flex items-center justify-between'>
+                                            <Dialog.Title className="w-full text-[#1075B2] text-xl text-center">
+                                                Добро пожаловать!
+                                            </Dialog.Title>
+                                            <button onClick={() => setIsModalOpen(false)}>
+                                                <button onClick={() => setIsModalOpen(false)}>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20"
+                                                         height="20" viewBox="0 0 50 50">
+                                                        <path
+                                                            d="M 7.7070312 6.2929688 L 6.2929688 7.7070312 L 23.585938 25 L 6.2929688 42.292969 L 7.7070312 43.707031 L 25 26.414062 L 42.292969 43.707031 L 43.707031 42.292969 L 26.414062 25 L 43.707031 7.7070312 L 42.292969 6.2929688 L 25 23.585938 L 7.7070312 6.2929688 z"></path>
+                                                    </svg>
+                                                </button>
+                                            </button>
+                                        </div>
+                                        <LogInForm onSignUpClick={openSignUpForm} onForgotClick={openForgotPasswordForm}
+                                                   setIsModalOpen={setIsModalOpen}/>
+                                    </>
+                                ),
+                                'forgot': (
+                                    <>
+                                        <Dialog.Title className="text-[#1075B2] text-xl mb-6 text-center">
+                                            Восстановить пароль
+                                        </Dialog.Title>
+                                        <ForgotPasswordForm onLogInClick={openLoginForm}/>
+                                    </>
+                                )
+                            }[formType]
+                        }
+                    </div>
+                </Dialog.Panel>
+            </Dialog>
+        </Transition>
     );
 }
 
